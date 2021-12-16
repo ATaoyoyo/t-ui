341f49b0140a8729ef22550d2e81393ee3ae3321
ATaoyoyo <1029187998@qq.com>
2021-12-16 18:04:13 +0800

    加入md及component插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import MarkDown from 'vite-plugin-md'
+import ViteComponents from 'vite-plugin-components'
 import { resolve } from 'path'
 
 const path = (name: string) => resolve(__dirname, name)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({ include: [/\.vue$/, /\.md$/] }),
+    MarkDown(),
+    // should be placed after `Markdown()`
+    ViteComponents({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+
+      // allow auto import and register components used in markdown
+      customLoaderMatcher: (path) => path.endsWith('.md'),
+    }),
+  ],
 
   css: {
     preprocessorOptions: {
