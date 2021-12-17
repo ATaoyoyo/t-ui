9770e1d0a8ad122748378412d643a0bceeb83d4d
ATaoyoyo <1029187998@qq.com>
2021-12-17 18:03:04 +0800

    去除了旧版文档依赖，打算用vitepress

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,12 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import MarkDown from 'vite-plugin-md'
-import ViteComponents from 'vite-plugin-components'
+import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 const path = (name: string) => resolve(__dirname, name)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({ include: [/\.vue$/, /\.md$/] }),
-    MarkDown(),
-    // should be placed after `Markdown()`
-    ViteComponents({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-
-      // allow auto import and register components used in markdown
-      customLoaderMatcher: (path) => path.endsWith('.md'),
-    }),
-  ],
-
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          hack: `true; @import(reference) "${path('src/theme/common/var.less')}"`,
-        },
-        javascriptEnabled: true,
-      },
-    },
-  },
+  plugins: [vue()],
 
   define: {
     __VUE_PROD_DEVTOOLS__: true,
